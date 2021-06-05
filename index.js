89bd6a9379c303dbfd8d2a3aca5ca8e08621a0ce
Kirk Duchesne <36648041+kirkduchesne@users.noreply.github.com>
2021-06-05 12:45:11 -0400

    require cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 const books = require('./routes/books');
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+//app.use(cors());
 
 mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
